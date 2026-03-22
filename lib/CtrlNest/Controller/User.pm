5f18bd8d95abdf3e451eb458c14d85ea408dc12e
Tanase Daniel <tanasedaniel54@gmail.com>
2026-03-22 15:09:25 +0200

    This commit implements changes requested in issue#12

--- lib/CtrlNest/Controller/User.pm
+++ lib/CtrlNest/Controller/User.pm
@@ -5,6 +5,8 @@ use Mojo::Util 'md5_sum';
 use File::Path 'make_path';
 
 use CtrlNest::Helper::Constants;
+use CtrlNest::Helper::Pagination;
+use CtrlNest::Validator::Pagination;
 
 ################################################################################
 
@@ -32,6 +34,54 @@ sub profile {
     layout => 'default',
     title  => 'Profile',
     u      => $user
+  );
+}
+
+################################################################################
+
+# @brief Renders the settings users list page.
+#
+# @method GET
+#
+# @param page   - The page number of the table (for pagination).
+#        search - The keyword to be searched.
+#
+# @return
+#   - HTTP 200 (OK) Returns the rendered /user/users page (HTML).
+#
+sub users {
+  my $self = shift;
+
+  # Get all the parameters or default them if not existing
+  my $page   = $self->param('page');
+  my $search = $self->param('search');
+
+  # Replace with default values if invalid
+  $page   = 1  unless validate_page_number($page)->{status} == SUCCESS;
+  $search = '' unless validate_search_keyword($search)->{status} == SUCCESS;
+
+  # Get the paginated users list
+  my $users_rs = $self->db->resultset('User')->get_paginated($search, $page);
+
+  # Get pagination object
+  my $pagination = get_pagination($users_rs->pager, $page);
+
+  # Map the result set to hashref
+  my @users = map { {
+    full_name  => $_->first_name . ' ' . $_->last_name,
+    username   => $_->username,
+    email      => $_->email,
+    role       => $_->role,
+    created_at => $_->created_at->strftime('%d %h, %Y')
+  } } $users_rs->all;
+
+  # Render template "user/users.html.ep"
+  return $self->render(
+    layout         => 'default',
+    title          => 'Users',
+    users          => \@users,
+    search_keyword => $search,
+    pagination     => $pagination
   );
 }
 
